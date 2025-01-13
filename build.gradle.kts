680e042eda030204fdfc0ec316d21b3c3acdb57d
Sirika HILAIRE <sirika.hilaire@cgi.com>
2025-01-13 10:30:51 +0100

    chore : config ci/cd

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("jacoco")
 }
 
 group = "kat.siri"
@@ -38,4 +39,9 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+	}
 }
